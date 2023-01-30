7137fb65743533df5be6ec71c723937be19de7ed
Bdoor-K3 <79381393+Bdoor-K3@users.noreply.github.com>
2023-01-31 00:05:30 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.balltmr = New System.Windows.Forms.Timer(Me.components)
+        Me.ballimg = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.blktimr = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.scorelbl = New System.Windows.Forms.Label()
+        CType(Me.ballimg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(-1, 1)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(324, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(329, 0)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(168, 23)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Font = New System.Drawing.Font("Showcard Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button1.Location = New System.Drawing.Point(505, 1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(117, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'balltmr
+        '
+        Me.balltmr.Interval = 10
+        '
+        'ballimg
+        '
+        Me.ballimg.Image = Global.voiceBallGame.My.Resources.Resources.basketball2
+        Me.ballimg.Location = New System.Drawing.Point(21, 393)
+        Me.ballimg.Name = "ballimg"
+        Me.ballimg.Size = New System.Drawing.Size(73, 50)
+        Me.ballimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ballimg.TabIndex = 3
+        Me.ballimg.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.LightSalmon
+        Me.PictureBox1.Location = New System.Drawing.Point(455, 280)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(70, 164)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.SaddleBrown
+        Me.PictureBox2.Location = New System.Drawing.Point(545, 228)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(65, 216)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.OliveDrab
+        Me.PictureBox3.Location = New System.Drawing.Point(354, 347)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(69, 97)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'blktimr
+        '
+        Me.blktimr.Interval = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Silver
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Symbol", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(222, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(190, 47)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Game Over"
+        Me.Label1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Silver
+        Me.Button2.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.Color.DarkRed
+        Me.Button2.Location = New System.Drawing.Point(260, 176)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 40)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Restart"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'scorelbl
+        '
+        Me.scorelbl.AutoSize = True
+        Me.scorelbl.Font = New System.Drawing.Font("Showcard Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.scorelbl.ForeColor = System.Drawing.Color.DarkRed
+        Me.scorelbl.Location = New System.Drawing.Point(23, 42)
+        Me.scorelbl.Name = "scorelbl"
+        Me.scorelbl.Size = New System.Drawing.Size(19, 20)
+        Me.scorelbl.TabIndex = 7
+        Me.scorelbl.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LavenderBlush
+        Me.ClientSize = New System.Drawing.Size(622, 445)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.scorelbl)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.ballimg)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.MaximumSize = New System.Drawing.Size(638, 484)
+        Me.Name = "Form1"
+        Me.Text = "VoiceBallGame"
+        CType(Me.ballimg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ballimg As PictureBox
+    Friend WithEvents balltmr As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents blktimr As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents scorelbl As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports NAudio.Wave
+Imports NAudio.CoreAudioApi
+Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+
+Public Class Form1
+    Dim score As Integer = 0
+    Dim en As New MMDeviceEnumerator
+    Dim voices As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim recorder = New WaveIn()
+        recorder.StartRecording()
+        Dim device = en.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active)
+        ComboBox1.Items.AddRange(device.ToArray)
+        ComboBox1.SelectedIndex = My.Settings.Setting
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ComboBox1.Text <> Nothing Then
+            Dim selected As MMDevice = ComboBox1.SelectedItem
+            voices = selected.AudioMeterInformation.MasterPeakValue * 100
+            ProgressBar1.Value = voices
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        balltmr.Start()
+        blktimr.Start()
+        ballimg.Image = My.Resources.basketball
+        My.Settings.Setting = ComboBox1.SelectedIndex
+
+    End Sub
+    Private Sub balltmr_Tick(sender As Object, e As EventArgs) Handles balltmr.Tick
+        If voices > 25 Then
+            ballimg.Top += -2
+        Else
+            ballimg.Top += +2
+        End If
+        If ballimg.Top >= 394 Then
+            ballimg.Top += -2
+        End If
+        If ballimg.Top <= 24 Then
+            ballimg.Top += +2
+        End If
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub blktimr_Tick(sender As Object, e As EventArgs) Handles blktimr.Tick
+        PictureBox1.Left += -2
+        PictureBox2.Left += -2
+        PictureBox3.Left += -2
+        If PictureBox1.Left <= -67 Then
+            PictureBox1.Left = 615
+            PictureBox1.Top = Int(Rnd() * 230) + 103
+            score += 1
+            scorelbl.Text = score
+        End If
+        If PictureBox2.Left <= -67 Then
+            PictureBox2.Left = 615
+            PictureBox2.Top = Int(Rnd() * 230) + 103
+            score += 1
+            scorelbl.Text = score
+        End If
+        If PictureBox3.Left <= -67 Then
+            PictureBox3.Left = 615
+            PictureBox3.Top = Int(Rnd() * 230) + 103
+            score += 1
+            scorelbl.Text = score
+        End If
+        If ballimg.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            Label1.Visible = True
+            Button2.Visible = True
+            blktimr.Stop()
+            balltmr.Stop()
+            Timer1.Stop()
+            ballimg.Image = My.Resources.basketball2
+        End If
+        If ballimg.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            Label1.Visible = True
+            Button2.Visible = True
+            blktimr.Stop()
+            balltmr.Stop()
+            Timer1.Stop()
+            ballimg.Image = My.Resources.basketball2
+        End If
+        If ballimg.Bounds.IntersectsWith(PictureBox3.Bounds) Then
+            Label1.Visible = True
+            Button2.Visible = True
+            blktimr.Stop()
+            balltmr.Stop()
+            Timer1.Stop()
+            ballimg.Image = My.Resources.basketball2
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Controls.Clear()
+        InitializeComponent()
+        Form1_Load(e, e)
+        blktimr.Start()
+        Timer1.Start()
+        balltmr.Start()
+
+        ballimg.Image = My.Resources.basketball
+        score = 0
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+End Class
+
+
